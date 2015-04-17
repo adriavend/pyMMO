4bbbdd32e911334a0a020271f98dbb98508bd062
bachi <bachi@e286a2eb-27d9-4e5b-978e-19684faf5f29>
2015-04-17 14:15:27 +0000

    Agregadas algunas clases para poder hacer la pantalla del menu.

---
+++ trunk/MMO/cursor.py
@@ -0,0 +1,10 @@
+__author__ = 'Bachi'
+
+import pygame
+
+class cursor(pygame.Rect):
+    def __init__(self):
+        pygame.Rect.__init__(self, 0, 0, 1, 1)
+
+    def update(self):
+        self.left, self.top = pygame.mouse.get_pos()
